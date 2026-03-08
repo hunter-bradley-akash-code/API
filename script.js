dedc42ad59b905544f23d6e490f87355c71987a6
AKASH_HunterBradley <ua04012007@gmail.com>
2026-03-08 18:34:12 +0530

    Update scanURL function to use placeholder API key
    Replaced hardcoded API key with a placeholder.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function scanURL(){
 let url = document.getElementById("url").value;
 let result = document.getElementById("result");
 
-let apiKey = "105558086b5c6e773c7addae55800f6fc17cb5bddcb5f75bf3746ed2a876d5af";
+let apiKey = "YOUR API";
 
 result.innerText = "Scanning...";
 
@@ -28,5 +28,6 @@ console.log(error);
 result.innerText = "Error connecting to VirusTotal API.";
 
 }
+
 
 }
